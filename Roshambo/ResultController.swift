86ab418397324d688b693290b0c546d01289bfe5
akumar <akumar@central1.com>
2015-06-23 16:32:13 -0700

    first commit

---
+++ Roshambo/ResultController.swift
@@ -0,0 +1,15 @@
+//
+//  ResultController.swift
+//  Roshambo
+//
+//  Created by Amit Kumar on 2015-06-23.
+//  Copyright (c) 2015 Agranee Solutions Ltd. All rights reserved.
+//
+
+import UIKit
+
+
+class ResultController: UIViewController {
+    
+    
+}
---
+++ Roshambo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Roshambo
+//
+//  Created by Amit Kumar on 2015-06-23.
+//  Copyright (c) 2015 Agranee Solutions Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func rockClicked(sender: UIButton) {
+        
+    }
+
+}
+
